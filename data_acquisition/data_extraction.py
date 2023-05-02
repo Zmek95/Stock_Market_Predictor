b667467aaf87db73c27d6f33801cbd092c969718
Ziyad Mekhemer <22456560+Zmek95@users.noreply.github.com>
2023-05-02 16:07:27 -0400

    Exchange data is now combined into one dataframe and saved to a parquet file format

--- data_acquisition/data_extraction.py
+++ data_acquisition/data_extraction.py
@@ -4,6 +4,7 @@ import os
 import string
 import sys
 import zipfile
+import pandas as pd
 from kaggle.api.kaggle_api_extended import KaggleApi
 from pathlib import Path
 from pyprojroot import here as get_project_root
@@ -15,9 +16,33 @@ def download_kaggle_dataset(dataset_url: string, path: Path) -> None:
     api = KaggleApi()
     api.authenticate()
     print('Downloading files...')
-    #api.dataset_download_files(dataset_url, path=path)
+    api.dataset_download_files(dataset_url, path=path)
     print('Download complete!\nUnpacking archive...')
 
     with zipfile.ZipFile(path / 'stock-market-dataset.zip','r') as zip_ref:
         zip_ref.extractall(path=path)
     print('Extraction succesful!')
+
+
+def combine_exchange_data(data_path: Path) -> pd.DataFrame:
+    etf_path = data_path / 'etfs'
+    stock_path = data_path / 'stocks'
+    symbol_paths = []
+
+    for directory in [etf_path, stock_path]:
+        for file in directory.iterdir():
+            if file.is_file():
+                symbol_paths.append(file)
+    
+    symbol_dfs = []
+
+    for symbol_path in symbol_paths:
+        df = pd.read_csv(symbol_path)
+        df['Symbol'] = symbol_path.name[:-4]
+        symbol_dfs.append(df.copy(deep=True))
+
+    df_combined_symbols = pd.concat(symbol_dfs, ignore_index=True)
+    del symbol_dfs
+    df_labels = pd.read_csv(data_path / 'symbols_valid_meta.csv')[['Symbol', 'Security Name']]
+    
+    return df_labels.merge(df_combined_symbols, how='left', on='Symbol')
