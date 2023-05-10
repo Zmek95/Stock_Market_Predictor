6b6ea726adba115bdde7ffabb33f102bdc98bb23
Ziyad Mekhemer <22456560+Zmek95@users.noreply.github.com>
2023-05-10 11:19:24 -0400

    Moved all constants in data_acquisition to config file

--- data_acquisition/main.py
+++ data_acquisition/main.py
@@ -6,9 +6,11 @@ os.chdir(get_project_root())
 sys.path.append(str(get_project_root()))
 from pathlib import Path
 
+import data_acquisition.config as cfg
 
-if __name__ == '__main__':
-    with open(get_project_root() / Path('data_acquisition/kaggle.json'), 'r') as f:
+
+def main():
+    with open(cfg.KAGGLE_CREDS_PATH, 'r') as f:
         kaggle_creds = json.load(f)
 
     os.environ['KAGGLE_USERNAME'] = kaggle_creds['username']
@@ -16,9 +18,13 @@ if __name__ == '__main__':
    
     from data_acquisition.data_extraction import download_kaggle_dataset, combine_exchange_data
 
-    path=get_project_root() / Path('data_acquisition/data')
-    dataset_url = 'jacksoncrow/stock-market-dataset'
+    path = cfg.COMBINED_DATA_PATH
+    dataset_url = cfg.DATA_KAGGLE_URL
     download_kaggle_dataset(dataset_url=dataset_url, path=path)
     combined_df = combine_exchange_data(path)
     combined_df.to_parquet(path / 'combined_df.parquet')
+
+
+if __name__ == '__main__':
+    main()    
 
